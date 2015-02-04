236fed43843bc09ca7bd9f1a3060a7c84f10369f
Deen Gao <deengao@gmail.com>
2015-02-04 15:14:30 +0800

    add a button

--- src/com/example/apptest/MainActivity.java
+++ src/com/example/apptest/MainActivity.java
@@ -2,14 +2,35 @@ package com.example.apptest;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+	
+	Button button1 ;
+	TextView tv;
+	Context context;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		context = this.getApplicationContext();
+		button1 = (Button) this.findViewById(R.id.button1);
+		tv = (TextView) this.findViewById(R.id.textView1);
+		button1.setOnClickListener(new OnClickListener(){
+
+			public void onClick(View v) {
+				tv.setText("After Clicked");
+				Toast.makeText(context ,"onclick", Toast.LENGTH_LONG).show();;
+			}
+			
+		});
 	}
 
 	@Override
